89a2152c51c4fe814b577fd2599ac95847b28afd
ProgrammingIncluded <chencharlesh@gmail.com>
2022-07-30 19:05:28 -0700

    Update install scripts

--- tools/install.py
+++ tools/install.py
@@ -2,6 +2,8 @@
 Sets up help hooks for formatting code.
 """
 import os
+import sys
+import stat
 import shutil
 
 tool_root = os.path.dirname(__file__)
@@ -11,4 +13,9 @@ hook_root = os.path.join(project_root, ".git", "hooks")
 if not os.path.exists(hook_root):
     os.makedirs(hook_root)
 
+final_dst = os.path.join(hook_root, "pre-commit")
 shutil.copyfile(os.path.join(tool_root, "pre-commit"), os.path.join(hook_root, "pre-commit"))
+
+if sys.platform == "linux":
+    stat = os.stat(final_dst)
+    os.chmod(final_dst, state.st_mode | state.S_IEXEC)
