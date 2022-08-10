e4b9a916273f96e9d38a76111b4f8afe56ba8c0c
ProgrammingIncluded <chencharlesh@gmail.com>
2022-08-10 00:44:23 -0700

    Update fronted for search query

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -20,12 +20,6 @@ for (let k in ENDPOINT) {
     R_ENDPOINT[ENDPOINT[k]] = k;
 }
 
-// Maps which keys to search for a given endpoint.
-let ENDPOINT_SEARCHABLE = {};
-ENDPOINT_SEARCHABLE[ENDPOINT[ADDRESS_TABLE_IDX]] = ["line_one", "line_two", "line_three"];
-ENDPOINT_SEARCHABLE[ENDPOINT[PEOPLE_TABLE_IDX]] = ["first_name", "middle_name"];
-ENDPOINT_SEARCHABLE[ENDPOINT[HOUSEHOLD_TABLE_IDX]] = ["house_name"]
-
 // End point mapping
 let GEN_ENDPOINT_LOOKUP = {};
 let GET_ENDPOINT_LOOKUP = {};
@@ -35,7 +29,7 @@ for (const idx of TABLE_IDX) {
 }
 
 class SearchManager {
-    constructor(searchBar, searchSuggestion, searchButton, searchDarkener, tablesSearch=ENDPOINT) {
+    constructor(searchBar, searchSuggestion, searchButton, searchDarkener) {
         this.searchBar = searchBar;
         this.searchSuggestion = searchSuggestion;
         this.searchButton = searchButton;
@@ -46,15 +40,13 @@ class SearchManager {
         this.searchDarkener.hide();
         // We don't want to spam the server
         this.currentQuery = "";
-        this.tablesSearch = {...tablesSearch};
-        // Stores tables that are valid to be searched.
-        this.validSearches = {...ENDPOINT_SEARCHABLE};
 
         // Requires binding due to function reference.
         this.searchBar.keyup(
             (event) => {
                 if (event.key == "Escape") {
                     this.searchDarkener.hide();
+                    this.searchSuggestion.hide();
                     // Unfocus so that we can refocus if start typing.
                     this.searchDarkener.blur();
                     return;
@@ -69,35 +61,37 @@ class SearchManager {
 
         // We only want to hid if user focuses and already typed.
         this.searchBar.focus(this.determineHide.bind(this));
-        this.searchBar.blur(() => { this.searchDarkener.hide(); });
+        this.searchBar.blur(() => { this.searchDarkener.hide(); this.searchSuggestion.hide(); });
     }
 
     dispatchSearch() {
-        // Dispatches the search result to all available tables.
-        this.searchSuggestion.empty();
-        for (const tableName in this.validSearches) {
-            let idx = R_ENDPOINT[tableName];
-            let endpoint = "/" + GET_ENDPOINT_LOOKUP[idx] + "/";
-            let queries = this.validSearches[tableName];
-
-            for (let q of queries) {
-                $.get(endpoint + "/?page=0" + `&${q}=${this.currentQuery}`, (data) => {
-                    if (data["data"].length != 0) {
-                        console.log(data["data"]);
-                        this.searchSuggestion.append(data["data"]);
-                    }
-                })
-                .fail((d, textStatus, error) => {console.log(error);});
-            }
+        if (this.currentQuery.length < 3) {
+            return;
         }
+
+        // Dispatches the search result to all available tables.
+        console.log(this.currentQuery)
+        $.get(`/search?query=${this.currentQuery}`, (data) => {
+            this.searchSuggestion.empty();
+            console.log(data);
+            for (let table_key in data) {
+                let results = data[table_key];
+                for (let r of results) {
+                    this.searchSuggestion.append(`${table_key}: ${JSON.stringify(r)}<br /><br />`);
+                    this.searchSuggestion.show();
+                }
+            }
+        }).fail((d, textStatus, error) => {console.log(error);});
     }
 
     determineHide() {
         if (this.searchBar.val() == "") {
             this.searchDarkener.hide();
+            this.searchSuggestion.hide();
             return true;
         }
 
+        this.searchSuggestion.show();
         this.searchDarkener.show();
     }
 }
